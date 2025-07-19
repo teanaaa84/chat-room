87b899bc755bcf01ea49370bbe6ec253a46c0cb2
teana84 <tinabahreini@gmail.com>
2025-07-19 12:22:36 +0330

    اضافه کردن gitignore و حذف فایل‌های بی‌ربط از گیت

--- chat_room/build/lib/models/message.py
+++
@@ -1,8 +0,0 @@
-from data_structure.link_list import link_list
-class Message:
-    def __init__(self,message_id,sender,time,content):
-        self.id=message_id
-        self.sender=sender
-        self.time=time
-        self.content=content
-        self.replies =link_list()  # ← پرانتز بزار تا شی بسازه
--- chat_room/test_project.py/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-
-    def login(self, username, password):
